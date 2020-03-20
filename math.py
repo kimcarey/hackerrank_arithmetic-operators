31b1c702c74428efa67f99190e872e22b54cc18a
Kim Carey <kthomas06@gmail.com>
2020-03-20 14:15:13 -0700

    Solution for Hackerrank Arithmetic Operators challenge.

---
+++ math.py
@@ -0,0 +1,9 @@
+def solve_math_problems(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    solve_math_problems(a, b)
